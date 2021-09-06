cd7a82678ce69abc25a72884a651430975932139
Jonathan Shine <jonathanhshine@gmail.com>
2021-09-06 14:45:38 +0200

    imported useState from React and stored an array of dummy paintings in a state hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 import Home from './Components/Home';
 import PaintingContext from './Context/PaintingContext';
 
 function App() {
+  const [paintingsArray, setPaintingsArray] = useState([
+    {
+      title: "Mona Lisa",
+      artist: "Leonardo da Vinci",
+      year: 1503,
+      price: "100 Million",
+      img: "imageurl"
+    },
+    {
+      title: "Water Lilies",
+      artist: "Claude Monet",
+      year: 1920,
+      price: "20 Million",
+      img: "imageurl"
+    }
+  ]);
+
+  console.log(paintingsArray);
 
   return (
-    <PaintingContext.Provider>
+    <PaintingContext.Provider value={ paintingsArray }>
       <div className="App">
         <Home/>
       </div>
