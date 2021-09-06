41afc69f5c591209e02f418faf3bdc51b8b8d28e
Jonathan Shine <jonathanhshine@gmail.com>
2021-09-06 14:49:40 +0200

    created PaintingList compoment, in which we will use the stored context to render a list of paintings/created PaintingItem, which will display data on an individual painting

---
+++ client/src/Components/PaintingItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PaintingItem = () => {
+    return (
+        <div>
+            <h3>Painting Item</h3>
+        </div>
+    )
+}
+
+export default PaintingItem
---
+++ client/src/Components/PaintingList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PaintingList = () => {
+    return (
+        <div>
+            <h2>Paintings List</h2>
+        </div>
+    )
+}
+
+export default PaintingList
