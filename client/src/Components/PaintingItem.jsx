cf04d57e021d8ffcb9be3b2ea31923fb74be159b
Jonathan Shine <jonathanhshine@gmail.com>
2021-09-07 10:13:27 +0200

    passed the individual painting data from the map iteration in PaintingList to the PaintingItem as a prop, and rendered the extra information in the item component

--- client/src/Components/PaintingItem.jsx
+++ client/src/Components/PaintingItem.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-const PaintingItem = () => {
+const PaintingItem = ({ painting }) => {
+    console.log(painting);
     return (
         <div>
-            <h3>Painting Item</h3>
+            <h3>{painting.title}</h3>
+            <p>{painting.artist}</p>
+            <p>{painting.year}</p>
+            <p>{painting.price}</p>
         </div>
     )
 }
--- client/src/Components/PaintingList.jsx
+++ client/src/Components/PaintingList.jsx
@@ -1,18 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import PaintingContext from '../Context/PaintingContext';
+import PaintingItem from '../Components/PaintingItem';
 
 const PaintingList = () => {
     const paintings = useContext( PaintingContext );
+    const [painting, setPainting] = useState({});
+    const [togglePainting, setTogglePainting] = useState(false);
+
+    console.log(painting);
+
+    const handleClick = (item) => {
+        setPainting(item);
+        setTogglePainting(!togglePainting);
+    }
 
     return (
         <div>
             <h2>Paintings List</h2>
             <ul>
                 {paintings.map(item => {
-                    return <li>
-                        <strong>{item.title}</strong> by {item.artist}
+                    return <li onClick={(e) => handleClick(item)}>
+                        <p><strong>{item.title}</strong> by {item.artist}</p>
                     </li>
                 })}
+                {togglePainting ? <PaintingItem painting={ painting }/> : null}
             </ul>
         </div>
     )
