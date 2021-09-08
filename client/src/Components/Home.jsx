602a6f3276b2d047f3fd09cfc519907674effb70
1983shreekesh <gluck.shreekesh@gmail.com>
2021-09-08 11:30:01 +0200

    added basic styling in Home css

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import PaintingList from './PaintingList'
+import React from "react";
+import PaintingList from "./PaintingList";
+import "../Styles/home.css";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Welcome to the Famous Paintings Store!</h1>
-            <PaintingList/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Welcome to the Famous Paintings Store!</h1>
+      <PaintingList />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
